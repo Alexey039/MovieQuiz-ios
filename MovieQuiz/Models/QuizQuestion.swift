a9c083c4ec938b72de0077183b0095628066bf47
Alexey Shimanskii <ShimanskiiAlexey@yandex.ru>
2024-04-22 00:01:41 +0200

    Finish 6 sprint

--- MovieQuiz/Models/QuizQuestion.swift
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -3,7 +3,7 @@ import UIKit
  struct QuizQuestion {
     // строка с названием фильма.
     // совпадает с название картинки афиши фильма в Assets
-    let image: String
+    let image: Data
     // строка с вопросом о рейтинге фильма
     let text: String
     // булевое значение (true, false), правильный ответ на вопрос
--- MovieQuiz/Services/QuestionFactoryProtocol.swift
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -1,5 +1,11 @@
 import UIKit
 
+//protocol QuestionFactoryProtocol {
+//    func requestNextQuestion()
+//}
+
 protocol QuestionFactoryProtocol {
+    var delegate: QuestionFactoryDelegate? { get set }
     func requestNextQuestion()
+    func loadData()
 }
